6931ab4a8268d6c6ecf1d7dd4430003c94906684
Luiz Fellipe <110192027+luizfellips@users.noreply.github.com>
2023-03-15 17:46:11 -0300

    Update add-product.php

--- Development/add-product.php
+++ Development/add-product.php
@@ -28,9 +28,9 @@
                 <label for="Price">Price ($)</label>
             </div>
             <div class="col inputs">
-                <input type="text" name="sku" required>
-                <input type="text" name="name" required>
-                <input type="number" name="price" step="any" required>
+                <input type="text" name="sku" id="sku" required>
+                <input type="text" name="name" id="name" required>
+                <input type="number" name="price" step="any" id="price" required>
             </div>
         </div>
 
