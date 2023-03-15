85d4e649c3e56177dff1aee25a7eb860c18ffdaf
Luiz Fellipe <110192027+luizfellips@users.noreply.github.com>
2023-03-15 15:15:47 -0300

    Update config.php

--- Production/Module/config.php
+++ Production/Module/config.php
@@ -3,4 +3,3 @@ define('DB_USER'        , "id20455986_root");
 define('DB_PASSWORD'    , "A)LJ]hNuB8{ffrWU");
 define('DB_NAME'        , "id20455986_scandiweb_db");
 define('DB_DRIVER'      , "mysql");
-?>
